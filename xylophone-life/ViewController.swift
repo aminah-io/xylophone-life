87b80aa62a73fd0de684110cf0dd3600bee420b2
Aminah Burch <hello@aminah.io>
2020-06-24 16:19:11 -0700

    Initial Commit

---
+++ xylophone-life/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xylophone-life
+//
+//  Created by Aminah Jamil on 6/24/20.
+//  Copyright © 2020 Aminah Jamil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
